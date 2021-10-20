0e74161a4ba17f40012f7c2cc38a4288b3f7c3c9
laloux.sophia <laloux.sophia@gmail.com>
2021-10-20 08:58:02 +0200

    [test 5vs5] test rouge

--- BehourdApp/BehourdApp.Test/TestSession.cs
+++ BehourdApp/BehourdApp.Test/TestSession.cs
@@ -3,6 +3,7 @@ using BehourdApp.Test.Mock;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BehourdApp.Test
 {
@@ -40,9 +41,19 @@ namespace BehourdApp.Test
 
             Partie partie = session.CreatePartie(joueurs5vs5);
 
-            Assert.IsNotNull(session);
-            Assert.IsNotNull(partie);
-            Assert.IsNotNull(session.GetEquipes());
+            List<Equipe> equipes = session.GetEquipes();
+            Assert.AreEqual(2, equipes.Count);
+
+            Equipe equipe1 = equipes.First();
+            Equipe equipe2 = equipes.Last();
+
+            foreach(Joueur j1 in equipe1.Joueurs)
+            {
+                foreach(Joueur j2 in equipe2.Joueurs)
+                {
+                    Assert.AreNotSame(j1, j2);
+                }
+            }
 
         }
     }
