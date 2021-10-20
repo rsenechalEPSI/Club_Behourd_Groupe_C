fb5a4654bea2f276d9f2eb1ec66cd7d8b2d36695
rsenechalEPSI <romain.senechal1@epsi.fr>
2021-10-20 08:48:12 +0200

    Refactorisation Test Duel

--- BehourdApp/BehourdApp.Test/TestSession.cs
+++ BehourdApp/BehourdApp.Test/TestSession.cs
@@ -3,6 +3,7 @@ using BehourdApp.Test.Mock;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BehourdApp.Test
 {
@@ -26,10 +27,36 @@ namespace BehourdApp.Test
 
             Partie partie = session.CreatePartie(joueurs);
 
-            Assert.IsNotNull(session);
-            Assert.IsNotNull(partie);
-            Assert.IsNotNull(session.GetEquipes());
+            List<Equipe> equipes = session.GetEquipes();
+
+            Assert.AreEqual(2, equipes.Count);
+
+            Joueur j1 = joueurs.First();
+            Joueur j2 = joueurs.Last();
+            Assert.AreNotSame(j1, j2);
+
             
+        }
+
+        [TestMethod]
+        public void Creation_10_vs_10()
+        {
+
+            List<Joueur> joueurs = ExcelData.JoueursBuilder(20);
+
+            Session session = new Session();
+
+            Partie partie = session.CreatePartie(joueurs);
+
+            List<Equipe> equipes = session.GetEquipes();
+
+            Assert.AreEqual(2, equipes.Count);
+
+            Joueur j1 = joueurs.First();
+            Joueur j2 = joueurs.Last();
+            Assert.AreNotSame(j1, j2);
+
+
         }
     }
 }
