8075630cbe88d3fa92a68eb2b5058f7ac3afbb04
Clément <Clment@DESKTOP-R2586JT>
2021-10-20 09:07:46 +0200

    test équilibrage rouge pas fini

--- BehourdApp/BehourdApp.Test/TestSession.cs
+++ BehourdApp/BehourdApp.Test/TestSession.cs
@@ -1,8 +1,8 @@
-﻿using BehourdApp.ConsoleApp.Classes;
-using BehourdApp.Test.Mock;
+﻿using BehourdApp.Test.Mock;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using BehourdApp.ConsoleApp;
 
 namespace BehourdApp.Test
 {
@@ -30,6 +30,36 @@ namespace BehourdApp.Test
             Assert.IsNotNull(partie);
             Assert.IsNotNull(session.GetEquipes());
             
+        }
+
+        public static List<List<T>> GetAllCombos<T>(List<T> list)
+        {
+            int comboCount = (int)Math.Pow(2, list.Count) - 1;
+            List<List<T>> result = new List<List<T>>();
+            for (int i = 1; i < comboCount + 1; i++)
+            {
+                // make each combo here
+                result.Add(new List<T>());
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if ((i >> j) % 2 != 0)
+                        result.Last().Add(list[j]);
+                }
+            }
+            return result;
+        }
+
+        [TestMethod]
+        public void Test_Equilibrage()
+        {
+            List<Joueur> joueurs = ExcelData.JoueursBuilder(11);
+
+            Session session = new Session();
+
+            Partie partie = session.CreatePartie(joueurs);
+
+            List<List<Joueur>> test = GetAllCombos(joueurs);
+            Assert.IsTrue(true);
         }
     }
 }
